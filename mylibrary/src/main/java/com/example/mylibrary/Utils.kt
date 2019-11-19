c4ff6e895db2fd9e0891069c4dc71d30af247b13
NurseyitTursunkulov <nurseyit.tursunkulov@gmail.com>
2019-11-19 12:19:24 +0600

    3

--- mylibrary/src/main/java/com/example/mylibrary/Utils.kt
+++ mylibrary/src/main/java/com/example/mylibrary/Utils.kt
@@ -1,11 +1,10 @@
 package com.example.mylibrary
 
 class Utils {
-    fun String.toBool():Boolean{
-        return this == "1"
-    }
-
     fun toBoolean(string : String):Boolean{
         return string == "1"
     }
+}
+fun String.toBool():Boolean{
+    return this == "1"
 }
